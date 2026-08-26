da5f037cecef3b55c8ed6e51bd9685be3fac17bb
Abolfazlmo15 <motamedabolfazzll2@gmail.com>
2026-08-27 03:15:22 +0330

    finalized

--- handlers/bot_handlers.py
+++ handlers/bot_handlers.py
@@ -1,10 +1,10 @@
 # handlers/bot_handlers.py
 from .base_handler import BaseHandler
-from .command_handlers import CommandHandlers
-from .message_handlers import MessageHandlers
-from .mode_handlers import ModeHandlers
-from .priority_handlers import PriorityHandlers
-from .cancel_handler import CancelHandler
+from .commands.command_handlers import CommandHandlers
+from .commands.message_handlers import MessageHandlers
+from .commands.mode_handlers import ModeHandlers
+from .commands.priority_handlers import PriorityHandlers
+from .commands.cancel_handler import CancelHandler
 
 class BotHandlers(
     BaseHandler,
